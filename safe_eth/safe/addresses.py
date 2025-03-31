37c7a5901970dcda89f0d38e9fb943bdf40aa772
ajimeno04 <jimenobeltran11@gmail.com>
2025-03-31 09:08:44 +0200

    feat: Rename JAPAN_OPEN_CHAIN_MAINNET to ASTAR_SHIBUYA and update related references in EthereumNetwork, BlockscoutClient, EtherscanClient, and addresses.py

--- safe_eth/safe/addresses.py
+++ safe_eth/safe/addresses.py
@@ -920,7 +920,7 @@ MASTER_COPIES: Dict[EthereumNetwork, List[Tuple[str, int, str]]] = {
         ("0xd9Db270c1B5E3Bd161E8c8503c55cEABeE709552", 85332, "1.3.0"),
         ("0x3E5c63644E683549055b9Be8653de26E0B4CD36E", 85331, "1.3.0+L2"),
     ],
-    EthereumNetwork.JAPAN_OPEN_CHAIN_MAINNET: [
+    EthereumNetwork.ASTAR_SHIBUYA: [
         ("0xfb1bffC9d739B8D520DaF37dF666da4C687191EA", 7709133, "1.3.0+L2"),
         ("0x69f4D1788e39c87893C980c06EdF4b7f686e2938", 7709135, "1.3.0"),
         ("0x41675C099F32341bf84BFc5382aF534df5C7461a", 13877078, "1.4.1"),  # v1.4.1
@@ -2568,9 +2568,9 @@ PROXY_FACTORIES: Dict[EthereumNetwork, List[Tuple[str, int]]] = {
     EthereumNetwork.BEAM_TESTNET: [
         ("0xa6B71E26C5e0845f74c812102Ca7114b6a896AB2", 85318),  # v1.3.0
     ],
-    EthereumNetwork.JAPAN_OPEN_CHAIN_MAINNET: [
+    EthereumNetwork.ASTAR_SHIBUYA: [
         ("0xC22834581EbC8527d974F8a1c97E1bEA4EF910BC", 7709119),  # v1.3.0
-        ("0x4e1DCf7AD4e460CfD30791CCC4F9c8a4f820ec67", 13877066),  # v1.4.1
+        ("0x4e1DCf7AD4e460CfD30791CCC4F9c8a4f820ec67", 5221009),  # v1.4.1
     ],
     EthereumNetwork.JAPAN_OPEN_CHAIN_TESTNET: [
         ("0xC22834581EbC8527d974F8a1c97E1bEA4EF910BC", 1315556),  # v1.3.0
